e43f6bbd184a97f0bff827764b7b194b981cdced
Curtis Rueden <ctrueden@wisc.edu>
2014-04-28 22:03:11 -0500

    Add interface for managing ImageJ UIs
    This is more specific than the SciJava UIService. But at the moment, it
    provides no additional API methods. Rather, it simply handles mouse
    presses in the main application frame, so that the status bar can be
    updated with the application details and memory use as expected.

---
+++ src/main/java/net/imagej/ui/DefaultImageJUIService.java
@@ -0,0 +1,79 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ui;
+
+import net.imagej.options.OptionsMemoryAndThreads;
+
+import org.scijava.app.AppService;
+import org.scijava.app.StatusService;
+import org.scijava.display.event.input.MsPressedEvent;
+import org.scijava.event.EventHandler;
+import org.scijava.options.OptionsService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.service.AbstractService;
+import org.scijava.service.Service;
+
+/**
+ * Default service for managing ImageJ user interfaces.
+ * 
+ * @author Curtis Rueden
+ */
+@Plugin(type = Service.class)
+public class DefaultImageJUIService extends AbstractService implements
+	ImageJUIService
+{
+
+	@Parameter
+	private OptionsService optionsService;
+
+	@Parameter
+	private StatusService statusService;
+
+	@Parameter
+	private AppService appService;
+
+	// -- Event handlers --
+
+	@EventHandler
+	protected void onEvent(final MsPressedEvent event) {
+		if (event.getDisplay() == null) {
+			final OptionsMemoryAndThreads options =
+				optionsService.getOptions(OptionsMemoryAndThreads.class);
+			if (options.isRunGcOnClick()) System.gc();
+			// mouse press in main application frame;
+			// show application info in status bar
+			statusService.showStatus(appService.getApp().getInfo(true));
+		}
+	}
+
+}
