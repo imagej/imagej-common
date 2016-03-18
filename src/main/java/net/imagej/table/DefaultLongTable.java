38c6a98584c1c853fd4d9a1d38d052df332d5b5c
Alison Walter <awalter2@wisc.edu>
2016-03-18 12:22:52 -0500

    Add table implementations for primitive types

---
+++ src/main/java/net/imagej/table/DefaultLongTable.java
@@ -0,0 +1,71 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.table;
+
+/**
+ * Default implementation of {@link LongTable}.
+ *
+ * @author Alison Walter
+ */
+public class DefaultLongTable extends AbstractTable<LongColumn, Long>
+	implements LongTable {
+
+	/** Creates an empty long table. */
+	public DefaultLongTable() {
+		super();
+	}
+
+	/** Creates a long table with the given row and column dimensions. */
+	public DefaultLongTable(final int columnCount, final int rowCount) {
+		super(columnCount, rowCount);
+	}
+
+	// -- LongTable methods --
+
+	@Override
+	public long getValue(final int col, final int row) {
+		return get(col).getValue(row);
+	}
+
+	@Override
+	public void setValue(final int col, final int row, final long value) {
+		get(col).setValue(row, value);
+	}
+
+	// -- Internal methods --
+
+	@Override
+	protected LongColumn createColumn(String header) {
+		return new LongColumn(header);
+	}
+
+}
