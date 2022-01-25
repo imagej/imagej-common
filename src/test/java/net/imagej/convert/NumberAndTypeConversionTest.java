c5fc9d163273b1eff67757d23377111746392a82
Gabriel Selzer <gjselzer@wisc.edu>
2022-01-25 16:29:17 -0600

    Clean up array
    We can make the Numbers much simpler

--- src/test/java/net/imagej/convert/NumberAndTypeConversionTest.java
+++ src/test/java/net/imagej/convert/NumberAndTypeConversionTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
-import net.imglib2.type.numeric.IntegerType;
 import net.imglib2.type.numeric.integer.*;
 import org.junit.After;
 import org.junit.Before;
@@ -28,18 +27,25 @@ public class NumberAndTypeConversionTest {
 	@Parameters(name="Type conversion test {index}: {0} <=> {1}")
 	public static Iterable<Object[]> data() {
 		return Arrays.asList(new Object[][] {
-				{new Double(Double.POSITIVE_INFINITY), new DoubleType(Double.POSITIVE_INFINITY)},
-				{new Float(Float.NEGATIVE_INFINITY), new FloatType(Float.NEGATIVE_INFINITY)},
-				{new Integer(Integer.MAX_VALUE), new IntType(Integer.MAX_VALUE)},
-				{new Long(Long.MAX_VALUE), new LongType(Long.MAX_VALUE)},
-				{new Integer(255), new UnsignedByteType(255)},
+				{ Double.POSITIVE_INFINITY, new DoubleType(Double.POSITIVE_INFINITY)},
+				{ Float.NEGATIVE_INFINITY, new FloatType(Float.NEGATIVE_INFINITY)},
+				{ Integer.MAX_VALUE, new IntType(Integer.MAX_VALUE)},
+				{ Long.MAX_VALUE, new LongType(Long.MAX_VALUE)},
+				{ 255, new UnsignedByteType(255)},
 				// Do one for each RealType
-				{new Byte((byte) 0), new ByteType((byte) 0)},
-				{new Integer(0), new IntType(0)},
-				{new Long(0), new LongType(0l)},
-				{new Short((short) 0), new ShortType((short) 0)},
-				{new Double(0.), new DoubleType(0.)},
-				{new Float(0.), new FloatType(0f)}
+				{ 0, new UnsignedByteType((byte) 0)},
+				{ 0L, new Unsigned2BitType((byte) 0)},
+				{ 0L, new Unsigned4BitType((byte) 0)},
+				{ 0L, new Unsigned12BitType((byte) 0)},
+				{ 0L, new UnsignedIntType((byte) 0)},
+				{ 0, new UnsignedShortType((byte) 0)},
+				{ 0L, new UnsignedLongType((byte) 0)},
+				{ (byte) 0, new ByteType((byte) 0)},
+				{ 0, new IntType(0)},
+				{ 0L, new LongType(0L)},
+				{ (short) 0, new ShortType((short) 0)},
+				{ 0., new DoubleType(0.)},
+				{ 0.F, new FloatType(0f)}
 			});
 	}
 
